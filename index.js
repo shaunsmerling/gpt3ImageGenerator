3a8d4e151e97b0c338ea7f6ad49d00500c8b2b1b
shaunsmerling <shaunsmerling@gmail.com>
2023-02-10 13:54:28 +0800

    simple frontend UI built

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const { urlencoded } = require("express");
 const express = require("express")
 const dotenv = require("dotenv").config()
 const port = process.env.PORT || 4000;
+const path = require("path")
 
 
 const app = express()
 
+// enable body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', require('./routes/openaiRoutes'))
 
